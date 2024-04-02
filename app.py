670456866cf89696f1299fdf736fa02277aba0e4
justForFun177 <165027910+justForFun177@users.noreply.github.com>
2024-04-02 19:01:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import  numpy as np
 import difflib
 import pickle
+import sklearn
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
